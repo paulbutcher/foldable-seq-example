34397f7f5e5e8a8e7a1a58f9c815121b24838d2c
Paul Butcher <paul@paulbutcher.com>
2013-03-13 15:12:47 +0000

    Update to Clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject foldable-seq "1.0"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.xml "0.0.7"]
-                 [foldable-seq "0.1"]]
+                 [foldable-seq "0.2"]]
   :main wordcount.core
   :jvm-opts ["-Xms4G" "-Xmx4G" "-XX:NewRatio=8"])
